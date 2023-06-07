7742e19d608e7355dfa0ddbd46b6e68f237a0457
LogicalAlmond <logicalalmond@protonmail.com>
2023-06-07 16:11:46 -0700

    add function to parse out cdid

--- cache.py
+++ cache.py
@@ -67,3 +67,21 @@ class WebCache:
         processed_data = pd.DataFrame()
         processed_data['text'] = interim_data_df['title'] + ' ' + interim_data_df['pagetext'] + ' ' + interim_data_df['source']
         processed_data.to_csv(OUTPUT_FILE, index=False, header=['text'])
+        
+    
+    def parse_cdid(self, interim_csv_file):
+        OUTPUT_FILE = 'cdid.csv'
+        OUTPUT_DIR = 'data/cdid'
+        CWD = os.getcwd()
+        
+        interim_data = pd.read_csv(interim_csv_file)
+        interim_data_df = pd.DataFrame(interim_data)
+        
+        if (CWD != os.path.join(os.getcwd(), OUTPUT_DIR)):
+            os.chdir(os.path.join(os.getcwd(), OUTPUT_DIR))
+        
+        cdid = interim_data_df['cdid']
+        cdid.to_csv(OUTPUT_FILE, index=False, header=['cdid'])
+        
+grab_data = WebCache()
+grab_data.parse_cdid('data/interim/interim_data.csv')
